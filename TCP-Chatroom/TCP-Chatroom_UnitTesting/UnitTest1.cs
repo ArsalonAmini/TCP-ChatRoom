52d5ccba0a1aafc2af538cbc5c56a9ee2ffa841b
ArsalonAmini <aminihajibasa@gmail.com>
2016-08-29 18:15:48 -0500

    UML diagrams for server/client as well as unit test psedocode for server/client

---
+++ TCP-Chatroom/TCP-Chatroom_UnitTesting/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TCP_Chatroom_UnitTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //testmethod for recievemessage
+            //string IsEqualTo result
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+
+            //TestMethod for recievemessage
+            //int IsEqualtTo result
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            //Testmethod for Sendmessage
+            //IsNotEqualTo 
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            //
+            //
+        }
+
+
+    }
+}
---
+++ TCP-Server/TCP-ServerTesting/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TCP_ServerTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Test Search method
+            //IsequalTo for search query result
+        }
+
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            //Test Chat Method
+            //IsEqualto for chat recieved message
+        }
+
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            //Test Private Chat Class
+            //IsNotEqual To for chat recieved public message
+        }
+
+    }
+}
