b8a4e50807e4187ccb509a28af30a53037f1c586
ArsalonAmini <aminihajibasa@gmail.com>
2016-08-31 11:48:43 -0500

    updates on the TCP Client/Server class, chat functioning semi function, NetworkStreamClass

--- TCP-Server/TCP-Client/Client.cs
+++ TCP-Server/TCP-Client/Client.cs
@@ -12,34 +12,40 @@ namespace TCP_Client
     {
         String server;
         String message;
+        IPAddress localAddress = IPAddress.Parse("10.2.20.26");
         Int32 port = 10000;
         TcpClient client;
         string responseData = string.Empty; //string to store ASCII response
+   
         public Client()
         {
-            
-            TcpClient client = new TcpClient(server, port); //constructor
+            client = new TcpClient("10.2.20.26", port); //constructor
         }
         public void Connect ()
         {
+            Console.WriteLine("Enter Message:");
+            message = Console.ReadLine();
+            //message = "Hello from client";
 
-            Byte[] data = System.Text.Encoding.ASCII.GetBytes(message); //translate passed message into ASCII and store as a byte array
+            Byte[] data = System.Text.Encoding.ASCII.GetBytes(message); //converts string message to ASCII zeros and ones code, stores ints into byte array called data
 
-            NetworkStream stream = client.GetStream(); //client stream ready for reading/writing
+            NetworkStream stream = client.GetStream(); //create variable stream, type networkstream, assign return client stream
+            
 
-            stream.Write(data, 0, data.Length); //send message to connected TcpServer
+            stream.Write(data, 0, data.Length); //send message to connected TcpServer //don't know what this line does
     
-            Console.WriteLine("Sent: {0}", message);
+            Console.WriteLine("Sent: {0}", message); //sends 
 
-            data = new Byte[256]; //buffer to store response bytes
+            //receiving own data // how to recieve data from server 
+            data = new Byte[256]; //buffer to store response bytes (response from server)
 
-            Int32 bytes = stream.Read(data, 0, data.Length); //store incoming data
+            Int32 bytes = stream.Read(data, 0, data.Length);  //read server data to stream client stream
 
-            responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes); //convert data to bytes
+            responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes); //convert server bytes to client string
             Console.WriteLine("Recieved: {0}", responseData);
 
-            stream.Close(); //close streaming connection 
-            client.Close(); //close client 
+            //stream.Close(); //close streaming connection 
+            //client.Close(); //close client 
       
         }
 
--- TCP-Server/TCP-Server/Server.cs
+++ TCP-Server/TCP-Server/Server.cs
@@ -14,58 +14,50 @@ namespace TCP_Server
         public byte[] buffer = new byte[BufferSize];
         int i;
         string data;
-        //Int32 port = 1300;
-        IPAddress localAddress = IPAddress.Parse("10.2.20.253");
+        Int32 port = 10000;
+        IPAddress localAddress = IPAddress.Parse("10.2.20.26");
         TcpListener server;
         //NetworkStream stream;
 
         public Server()
         {
-            TcpListener server = new TcpListener(localAddress, 1000); //constructor //transmission control server
+            server = new TcpListener(localAddress, port); //constructor //transmission control server
             data = null;
         }
 
-        public void AcceptTcpClient()
-        {
-            server.AcceptTcpClient();
-        }
-
-        public void AcceptSocket()
-        {
-            server.AcceptSocket();
-        }
-
+     
         public void Start()
         {
-            byte[] bytes = new Byte[1024]; //buffer for reading data
+            byte[] bytes = new Byte[1024]; //buffer for reading data                          
             server.Start(); //start listening for client requests
 
             while (true)
             {
                 Console.WriteLine("Waiting for a connection...");
-                TcpClient client = server.AcceptTcpClient();
+                TcpClient client = server.AcceptTcpClient(); //creating a member variable, type TcPclient to store accepted client 
                 Console.WriteLine("Connected!");
-
-                NetworkStream stream = client.GetStream(); //instantiating/assigning stream object for reading and writing
+                NetworkStream stream = client.GetStream(); //creating a member variable, type Networkstream to store client stream
 
                 while ((i = stream.Read(bytes, 0, bytes.Length)) != 0) //reiceve client data loop
                 {
-                    data = System.Text.Encoding.ASCII.GetString(bytes, 0, i); //translate data bytes to ASCII string
+                    //data = System.Text.Encoding.ASCII.GetString(bytes, 0, i); //translate data bytes to ASCII string
+                    data = "hello";
                     Console.WriteLine("Sent: {0}", data);
-
-                    data = data.ToUpper(); //processing data sent by client
 
                     byte[] message = System.Text.Encoding.ASCII.GetBytes(data);
 
                     //send message back
                     stream.Write(message, 0, message.Length);
-                    Console.WriteLine("Sent: {0}", data);
+                    Console.WriteLine("Recieved: {0}", data);
+                    Console.ReadLine();
 
-                    client.Close(); //shutdown and end connection
                 }
-                server.Stop(); //stop listening for new clients
+
+                    server.Stop(); //stop listening for new clients
+                    client.Close(); //shutdown and end connection
             }
 
+                
         }
     }
 }
